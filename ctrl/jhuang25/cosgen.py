282a7daa82ef8d8370d6796bf93c3e974b7ef950
Jonathan Huang <jonathanis3.1415@gmail.com>
2024-12-08 21:58:44 -0500

    fixed cosine lookup

--- ctrl/jhuang25/cosgen.py
+++ ctrl/jhuang25/cosgen.py
@@ -4,12 +4,12 @@ I = 160
 
 for i in range(2*I):
     val = cos(i * pi / I) / I
-    hex = f"{(int(val*(1<<31))):08x}"
+    hex = f"{(int(val*(1<<31)) + (1<<31)):08x}"
     # print(hex)
     # because things are so small they never come close to using
     # the first digit, so we are ok to check first digit for sign
-    if hex[0] == '-':
-        hex = '0' + hex[1:]
-    elif hex[0] == '0' and i != 80 and i != 240: # should not be 0
+    if hex[0] == '0' and i != 80 and i != 240: # keep 0
         hex = '8' + hex[1:]
+    elif hex[0] == '8':
+        hex = '0' + hex[1:]
     print(f"assign lookup[{i}] = 32'h{hex}; // {i} {val}")
