b9703678aed0a959380fce3997ad06e529823569
Jonathan Huang <jonathanis3.1415@gmail.com>
2024-12-08 22:51:31 -0500

    i swear i know how twos complement owrk

--- ctrl/jhuang25/cosgen.py
+++ ctrl/jhuang25/cosgen.py
@@ -4,12 +4,10 @@ I = 160
 
 for i in range(2*I):
     val = cos(i * pi / I) / I
-    hex = f"{(int(val*(1<<31)) + (1<<31)):08x}"
-    # print(hex)
-    # because things are so small they never come close to using
-    # the first digit, so we are ok to check first digit for sign
-    if hex[0] == '0' and i != 80 and i != 240: # keep 0
-        hex = '8' + hex[1:]
-    elif hex[0] == '8':
-        hex = '0' + hex[1:]
-    print(f"assign lookup[{i}] = 32'h{hex}; // {i} {val}")
+    hex = format(int(val * (1<<31) + (1<<31)), '032b')
+    chars = [hex[j] for j in range(32)]
+    chars[0] = '1' if chars[0] == '0' else '0' # flip
+    res = ""
+    for j in range(32):
+        res += chars[j]
+    print(f"assign lookup[{i}] = 32'h{int(res,2):08x}; // {i} {val}")
